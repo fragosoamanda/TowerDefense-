a2b6d654c549075c39ada1457adfb7a5745e96e2
Amanda <fragosoamanda30@gmail.com>
2025-05-29 15:01:33 -0300

    inclusion of .h

--- code/Tower.h
+++ code/Tower.h
@@ -1,11 +1,13 @@
 #ifndef TOWER_H
 #define TOWER 
+#include "Posicao.h"
+#include "Enemy.h"
 #include <time.h>
 
 class Tower{
 private:
     int dano;
-    float alcance;a
+    float alcance;
     float TempRecarga;  //tempo entre 2 ataques consecutivos
     float UltimoAtck;   //ultimo ataque para controlar a recarga
     Posicao posicao;    //posição no mapa
@@ -13,8 +15,9 @@ private:
 public: 
     Tower(int dano, float alcance, float TempRecarga, const Posicao& posicao);
     bool podeAtacar();              // verifica se pode atacar 
-    void atacar(Inimigo& inimigo);  // ataca o inimigo se estiver no range
-    float distancia(const posicao& p1, const posicao& p2); // calcula a distancia entre dois pontos para saber se o inimigo esta no range
+    
+    void atacar(Enemy& Enemy);  // ataca o inimigo se estiver no range
+    float distancia(const Posicao& p1, const Posicao& p2); // calcula a distancia entre dois pontos para saber se o inimigo esta no range
     Posicao getPosicao() const;     // retorna a posição 
 };
 #endif 
